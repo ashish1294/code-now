713a266dd44535d4722f3c4cc68b2716125dc03d
ashish1294 <ashish1294@gmail.com>
2014-09-12 19:10:30 +0530

    Admin Privileges in install script removed Chromium Auto-Install Feature removed Manifest file changed to achieve platform independence

--- host-program/install.sh
+++ host-program/install.sh
@@ -3,8 +3,9 @@
 ROOT_UID="0"
 
 #Check if run as root
-if [ "$UID" -ne "$ROOT_UID" ] ; then
-	echo "Please Run the script as sudo !!"
+if [ "$UID" -eq "$ROOT_UID" ] ; then
+	echo "Root Privileges Detected....!!"
+	echo "Please Run this script without root privileges"
 	exit 127
 fi
 
@@ -41,18 +42,9 @@ fi
 if [ $ischrome -eq 0 ] && [ $ischromium -eq 0 ]
 then
 	echo "Neither Google Chrome nor chromium was found !! Please download and install google chrome from https://www.google.com/chrome/browser/"
-	echo "\n\n"
-	echo "Do You want to automatically install chromium browser now ??? [Y/y for yes, Any other input for no] : "
-	read inst_ch
-	if [ $inst_ch -eq 'Y' ] || [ $inst_ch -eq 'y']
-	then
-		apt-get install chromium-browser
-		ischromium=1
-	else
-		echo "Installation Incomplete"
-		echo "Terminating ............!!"
-		exit 127
-	fi
+	echo "Installation Incomplete"
+	echo "Terminating ............!!"
+	exit 127
 fi
 
 echo "The installation folder comes with 3 template files for C, C++ and Java. All new codes will contain their repective templates. Use these template files to include/import all the required header files and classes. Enter Y or y if you want to continue:"
@@ -121,67 +113,46 @@ fi
 
 #Creating Required Directories
 
-if [ $ischromium -eq 1 ]
-then
-	path_dir="$HOME/.config/chromium/NativeMessagingHosts"
-	mkdir -p $path_dir/code-now/
-	rm -r "$path_dir/code-now"
-	mkdir -p $path_dir/code-now/
-	json_file="$path_dir/codenow.json"
-	json_file2="/etc/opt/chrome/native-messaging-hosts/codenow.json"
-	mkdir -p /etc/opt/chrome/native-messaging-hosts > /dev/null 2>&1
-
-	if [ -f $json_file ]
-	then
-		rm $json_file
-	fi
-
-	if [ -f $json_file2 ]
-	then
-		rm $json_file2
-	fi
-fi
-
-if [ $ischrome -eq 1 ]
-then
-	path_dir="$HOME/.config/google-chrome/NativeMessagingHosts"
-	mkdir -p $path_dir/code-now/
-	rm -r "$path_dir/code-now"
-	mkdir -p $path_dir/code-now/
-	json_file="$path_dir/codenow.json"
-	json_file2="/etc/opt/chrome/native-messaging-hosts/codenow.json"
-	mkdir -p /etc/opt/chrome/native-messaging-hosts > /dev/null 2>&1
-
-	if [ -f $json_file ]
-	then
-		rm $json_file
-	fi
-
-	if [ -f $json_file2 ]
-	then
-		rm $json_file2
-	fi
-fi
+mkdir -p $HOME/.code-now > /dev/null 2>&1
+rm $HOME/.code-now/* > /dev/null 2>&1
 
 # Creating the Json Manifest File
-(cat codenow.json | sed -e "s:PATH_TO_REQ_DIR:$path_dir:g") > $path_dir/codenow.json
-cp $path_dir/codenow.json /etc/opt/chrome/native-messaging-hosts/
+path_dir=$HOME
+if [ $ischrome -eq 1 ]
+then
+	json_file="$HOME/.config/google-chrome/NativeMessagingHosts/codenow.json"
+	if [ -f $json_file ]
+	then
+		rm $json_file
+	fi
+	(cat codenow.json | sed -e "s:PATH_TO_REQ_PROG:$path_dir/.code-now/prog.py:g") > $HOME/.config/google-chrome/NativeMessagingHosts/codenow.json
+fi
+
+if [ $ischromium -eq 1 ]
+then
+	json_file="$HOME/.config/chromium/NativeMessagingHosts/codenow.json"
+	if [ -f $json_file ]
+	then
+		rm $json_file
+	fi
+	(cat codenow.json | sed -e "s:PATH_TO_REQ_PROG:$path_dir/.code-now/prog.py:g") > $HOME/.config/chromium/NativeMessagingHosts/codenow.json
+fi
+
 echo "JSON file created .........................."
 
 # Creating the prog.py script
-cp prog.py $path_dir/code-now/prog.py
-sed "s:PATH_TO_EXEC_SCRIPT:$path_dir/code-now/host_program.sh:g" prog.py > $path_dir/code-now/prog.py
-sed "s:DEFAULT_SOLUTION_PATH:$sol_path:g" $path_dir/code-now/prog.py > $path_dir/code-now/prog.py.tmp && mv $path_dir/code-now/prog.py.tmp $path_dir/code-now/prog.py
-sed "s:JAVA_IDE:$javaide:g" $path_dir/code-now/prog.py > $path_dir/code-now/prog.py.tmp && mv $path_dir/code-now/prog.py.tmp $path_dir/code-now/prog.py
-sed "s:CPP_IDE:$cppide:g" $path_dir/code-now/prog.py > $path_dir/code-now/prog.py.tmp && mv $path_dir/code-now/prog.py.tmp $path_dir/code-now/prog.py
-sed "s:C_IDE:$cide:g" $path_dir/code-now/prog.py > $path_dir/code-now/prog.py.tmp && mv $path_dir/code-now/prog.py.tmp $path_dir/code-now/prog.py
-chmod +x $path_dir/code-now/prog.py
+cp prog.py $path_dir/.code-now/prog.py
+sed "s:DEFAULT_SOLUTION_PATH:$sol_path:g" $path_dir/.code-now/prog.py > $path_dir/.code-now/prog.py.tmp && mv $path_dir/.code-now/prog.py.tmp $path_dir/.code-now/prog.py
+sed "s:JAVA_IDE:$javaide:g" $path_dir/.code-now/prog.py > $path_dir/.code-now/prog.py.tmp && mv $path_dir/.code-now/prog.py.tmp $path_dir/.code-now/prog.py
+sed "s:CPP_IDE:$cppide:g" $path_dir/.code-now/prog.py > $path_dir/.code-now/prog.py.tmp && mv $path_dir/.code-now/prog.py.tmp $path_dir/.code-now/prog.py
+sed "s:C_IDE:$cide:g" $path_dir/.code-now/prog.py > $path_dir/.code-now/prog.py.tmp && mv $path_dir/.code-now/prog.py.tmp $path_dir/.code-now/prog.py
+chmod +x $path_dir/.code-now/prog.py
 echo "Python Script Created ......................"
 
 # Copying the template files
-cp c_template.c "$path_dir/code-now/"
-cp cpp_template.cpp "$path_dir/code-now/"
-cp java_template.java "$path_dir/code-now/"
-echo "Templates Copied"
+cp c_template.c $path_dir/.code-now/
+cp cpp_template.cpp $path_dir/code-now/
+cp java_template.java $path_dir/.code-now/
+echo "Templates Copied............................"
 
 echo "Installation Successful !! Please Install the extension"
