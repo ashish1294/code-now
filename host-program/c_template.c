56a3d516173eb503cbff9b1c4ecd31cf5c327f38
Ashish Kedia <ashish1294@gmail.com>
2015-01-04 16:56:48 +0530

    Edited Default Template Added git ignore

--- host-program/c_template.c
+++ host-program/c_template.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
+#include<string.h>
+#include<limits.h>
+
 int main()
 {
     return 0;
--- host-program/cpp_template.cpp
+++ host-program/cpp_template.cpp
@@ -1,7 +1,4 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
-#include<utility>
+#include<bits/stdc++.h>
 
 using namespace std;
 
