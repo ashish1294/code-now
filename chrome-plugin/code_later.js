8df72b4d7b7e0a372582ded533e45f3af1e12fc2
Ashish kedia <ashish1294@gmail.com>
2015-07-07 16:53:53 -0700

    Codeforces Support Added

--- chrome-plugin/code_later.js
+++ chrome-plugin/code_later.js
@@ -1,5 +1,5 @@
 //Getting the URL of the HTMl Page
-url = document.URL
+var url = document.URL
 
 //Removing the trailing punctuation marks
 while(url[url.length - 1] === "/" || url[url.length - 1] === "#")
@@ -8,13 +8,42 @@ while(url[url.length - 1] === "/" || url[url.length - 1] === "#")
 }
 
 //Spliting the URL to get path
-us = url.split("/");
+var us = url.split("/");
+
+var website = us[2].split(".")
+for(i = 0; i < website.length; i++)
+{
+    if (website[i] === "codechef" || website[i] === "spoj" || website[i] === "codeforces")
+    {
+        website = website[i];
+    }
+}
+
+function click_codeforces(language)
+{
+    console.log(language + " Code Now Clicked. Opening " + language + " IDE");
+    var prob_name = document.getElementById('pageContent').getElementsByClassName('title')[0].innerHTML.split('.')[1].trim();
+    var prob_url = url;
+    var u_name = "";
+    var head_div = document.getElementById('header').getElementsByClassName('lang-chooser')[0].getElementsByTagName('div')[1];
+    var reg_link = head_div.getElementsByTagName('a')[1].innerHTML.toLowerCase();
+    if(reg_link == "register")
+    {
+        u_name = "No User";
+    }
+    else
+    {
+        u_name = head_div.getElementsByTagName('a')[0].innerHTML.toLowerCase();
+    }
+    chrome.runtime.sendMessage({problem_name: prob_name, problem_url: prob_url, user_name: u_name, lang : language});
+    console.log("IDE");
+}
 
 //Checking if it is CodeChef Problem Page
-if(us.length > 4 && us[2].split(".")[1] === "codechef" && us[us.length-2] === "problems")
+if(us.length > 4 && website === "codechef" && us[us.length - 2] === "problems")
 {
     //Excluding the Special Codes in CodeChef Problem Regex - These url indicate a list of problems rather than a particular problem
-    code = us[us.length - 1];
+    var code = us[us.length - 1];
     if(code !== "easy" && code !== "medium" && code !== "hard" && code !== "challenge" && code !== "extcontest" && code !== "school")
     {
         //Adding the Handlers Buttons to the Page
@@ -87,7 +116,7 @@ if(us.length > 4 && us[2].split(".")[1] === "codechef" && us[us.length-2] === "p
 }
 
 //Checking for Spoj Problem Pages
-else if(us.length > 4 && us[2].split(".")[1] === "spoj" && us[us.length-2] === "problems")
+else if(us.length > 4 && website === "spoj" && us[us.length - 2] === "problems")
 {
     //Again Avoiding Special URLs
     var code = us[us.length - 2];
@@ -101,6 +130,7 @@ else if(us.length > 4 && us[2].split(".")[1] === "spoj" && us[us.length-2] === "
         console.log("Code Now Button Added !! ~ Code Now Extension");
 
         document.getElementById("code_now_id_java_button").onclick = function(){
+            console.log("Java Code Now Clicked. Opening Java IDE");
             var prob_name = document.getElementsByClassName("prob")[0].getElementsByTagName("h2")[0].innerHTML;
             var prob_url = url;
             var u_name = "";
@@ -149,4 +179,35 @@ else if(us.length > 4 && us[2].split(".")[1] === "spoj" && us[us.length-2] === "
     }
 }
 
-//(TODO) Add CodeForces Checking Here
+//Checking for Codeforces Problem Page
+else if(us.length > 4 && website === "codeforces" && (us[us.length - 2] === "problem" || us[us.length - 3] === "problem"))
+{
+    //Creating and Adding A whole new Sidebox with 3 buttons
+    var sidebar = document.getElementById('sidebar')
+    sidebar.innerHTML = 
+    '<div class="roundbox sidebox" style="">'
+        + '<div class="roundbox-lt">&nbsp;</div>'
+        + '<div class="roundbox-rt">&nbsp;</div>'
+        + '<table class="rtable ">'
+            + '<tbody>'
+                + '<tr><th class="left" style="width:100%;"><a style="color: black" href="">Code Now</a></th></tr>'
+                + '<tr>'
+                    + '<td class="left bottom" colspan="1">'
+                      + '<div style="text-align:center;margin:1em;">'
+                        + '<button id="code_now_id_java_button">Code in Java</button> <br><br>'
+                        + '<button id="code_now_id_c_button">Code in C</button> <br><br>'
+                        + '<button id="code_now_id_c++_button">Code in C++</button> <br><br>'
+                      + '</div>'
+                    + '</td>'
+                + '</tr>'
+            + '</tbody>'
+        + '</table>'
+    + '</div>'
+    + sidebar.innerHTML;
+
+    console.log("Code Now Button Added !! ~ Code Now Extension");
+
+    document.getElementById("code_now_id_java_button").onclick = function() { click_codeforces("java"); };
+    document.getElementById("code_now_id_c_button").onclick = function(){ click_codeforces("c"); };
+    document.getElementById("code_now_id_c++_button").onclick = function(){ click_codeforces("cpp"); };
+}
